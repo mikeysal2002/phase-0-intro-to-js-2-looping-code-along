af94f6070656098818295d3562dc3dcfda8c5031
Michael <mikesalgd@gmail.com>
2024-09-10 12:29:23 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+function writeCards(name, event){
+const messages = [];
+
+    for (let i = 0; i < name.length; i++) {
+       const message = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(i){
+    var ret = i;
+    while (i >= 0){
+        console.log(i)
+        --i;
+    }
+
+    return ret;
+
+}
+
+
